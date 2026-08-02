9a54511beb65ddb325bb2cd319f2dbcf8e0b3c2c
raullopezrios3-afk <raullopezrios3@gmail.com>
2026-08-01 19:19:51 -0600

    Update CanvasManager.js

--- NEXIUM_CORE/js/canvas/CanvasManager.js
+++ NEXIUM_CORE/js/canvas/CanvasManager.js
@@ -1,1 +1,100 @@
+/*
+=========================================
+ NEXIUM CORE
+ Canvas Manager
+=========================================
+*/
 
+
+class CanvasManager{
+
+
+    constructor(core){
+
+        this.core = core;
+
+        this.canvas = null;
+
+        this.context = null;
+
+
+        console.log(
+            "Canvas Manager creado"
+        );
+
+    }
+
+
+
+    init(id){
+
+
+        this.canvas =
+        document.getElementById(id);
+
+
+
+        if(!this.canvas){
+
+            console.error(
+                "Canvas no encontrado:",
+                id
+            );
+
+            return;
+
+        }
+
+
+
+        this.context =
+        this.canvas.getContext("2d");
+
+
+
+        this.core.canvas =
+        this.canvas;
+
+
+        this.core.context =
+        this.context;
+
+
+
+        console.log(
+            "Canvas inicializado"
+        );
+
+
+    }
+
+
+
+    resize(ancho, alto){
+
+
+        if(!this.canvas)
+        return;
+
+
+
+        this.canvas.width =
+        ancho;
+
+
+        this.canvas.height =
+        alto;
+
+
+
+        console.log(
+            "Canvas redimensionado",
+            ancho,
+            alto
+        );
+
+
+    }
+
+
+}
