d173446e35265cbe581b3e72874f79003d22101f
raullopezrios3-afk <raullopezrios3@gmail.com>
2026-08-01 21:29:16 -0600

    Create BoundingBoxRenderer.js

---
+++ NEXIUM_CORE/js/interaction/BoundingBoxRenderer.js
@@ -0,0 +1,73 @@
+/*
+=========================================
+ NEXIUM CORE
+ Bounding Box Renderer
+=========================================
+*/
+
+class BoundingBoxRenderer{
+
+    constructor(core){
+
+        this.core = core;
+
+        console.log(
+            "Bounding Box Renderer creado"
+        );
+
+    }
+
+
+
+    render(){
+
+        const obj =
+        this.core.selectedObject;
+
+        if(!obj)
+        return;
+
+
+
+        const ctx =
+        this.core.context;
+
+
+
+        ctx.save();
+
+
+
+        ctx.strokeStyle =
+        "#2196F3";
+
+
+
+        ctx.lineWidth =
+        2;
+
+
+
+        ctx.setLineDash([6,4]);
+
+
+
+        ctx.strokeRect(
+
+            obj.x-2,
+
+            obj.y-2,
+
+            obj.ancho+4,
+
+            obj.alto+4
+
+        );
+
+
+
+        ctx.restore();
+
+    }
+
+}
