0b53291c682634d7deaf03f5b246b949ee0a8319
raullopezrios3-afk <raullopezrios3@gmail.com>
2026-08-03 23:31:50 -0600

    Add TransformManager class for managing transformations

---
+++ NEXIUM_CORE/js/managers/TransformManager.js
@@ -0,0 +1,31 @@
+/*
+=========================================
+ NEXIUM CORE
+ Transform Manager
+=========================================
+*/
+
+
+class TransformManager{
+
+
+    constructor(core){
+
+        this.core = core;
+
+
+        this.transforming = false;
+
+        this.objeto = null;
+
+        this.handle = null;
+
+
+        console.log(
+            "Transform Manager creado"
+        );
+
+    }
+
+
+}
