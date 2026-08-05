f13e1d6d3b91f1cd7b347f1bb3d119e88aaba5ac
raullopezrios3-afk <raullopezrios3@gmail.com>
2026-08-05 15:22:07 -0600

    Update ObjectActionsManager.js

--- NEXIUM_CORE/js/managers/ObjectActionsManager.js
+++ NEXIUM_CORE/js/managers/ObjectActionsManager.js
@@ -9,17 +9,19 @@
 class ObjectActionsManager{
 
 
-    constructor(core){
+   constructor(core){
 
-        this.core = core;
+    this.core = core;
 
 
-        console.log(
-            "Object Actions Manager creado"
-        );
+    this.clipboard = null;
 
-    }
 
+    console.log(
+        "Object Actions Manager creado"
+    );
+
+}
 
 
     /*
@@ -88,7 +90,7 @@ class ObjectActionsManager{
 
 
 
-    /*
+        /*
     =========================================
      DUPLICAR OBJETO SELECCIONADO
     =========================================
@@ -179,6 +181,155 @@ class ObjectActionsManager{
 
         console.log(
             "Objeto duplicado",
+            copia
+        );
+
+
+    }
+
+
+
+
+
+    /*
+    =========================================
+     COPIAR OBJETO SELECCIONADO
+    =========================================
+    */
+
+
+    copySelected(){
+
+
+        const obj =
+        this.core.selectedObject;
+
+
+
+        if(!obj){
+
+            console.log(
+                "No hay objeto seleccionado"
+            );
+
+            return;
+
+        }
+
+
+
+        this.clipboard =
+
+        JSON.parse(
+
+            JSON.stringify(obj)
+
+        );
+
+
+
+        console.log(
+            "Objeto copiado",
+            this.clipboard
+        );
+
+
+    }
+
+
+
+
+
+    /*
+    =========================================
+     PEGAR OBJETO
+    =========================================
+    */
+
+
+    paste(){
+
+
+        if(!this.clipboard){
+
+
+            console.log(
+                "Clipboard vacío"
+            );
+
+
+            return;
+
+        }
+
+
+
+        const copia =
+
+        JSON.parse(
+
+            JSON.stringify(
+                this.clipboard
+            )
+
+        );
+
+
+
+        /*
+        Nuevo ID
+        */
+
+        copia.id =
+        Date.now();
+
+
+
+        /*
+        Desplazamiento visual
+        */
+
+        copia.x += 30;
+
+        copia.y += 30;
+
+
+
+        /*
+        Agregar objeto
+        */
+
+        this.core.objects.push(
+            copia
+        );
+
+
+
+        /*
+        Seleccionar nueva copia
+        */
+
+        this.core.selectedObject =
+        copia;
+
+
+
+        /*
+        Guardar historial
+        */
+
+        if(
+            this.core.modules.history
+        ){
+
+            this.core.modules.history.save();
+
+        }
+
+
+
+        console.log(
+            "Objeto pegado",
             copia
         );
 
