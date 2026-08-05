47f3fd5381e5538e5d6785e1576e64496ba4b2a1
raullopezrios3-afk <raullopezrios3@gmail.com>
2026-08-05 15:16:10 -0600

    Update KeyboardManager.js

--- NEXIUM_CORE/js/core/KeyboardManager.js
+++ NEXIUM_CORE/js/core/KeyboardManager.js
@@ -25,102 +25,143 @@ class KeyboardManager{
 
 
 
-   init(){
+    init(){
 
 
-    window.addEventListener(
-        "keydown",
-        (e)=>{
+        window.addEventListener(
+            "keydown",
+            (e)=>{
 
 
-            /*
-            =========================
-             UNDO
-            =========================
-            */
-
-
-            if(
-                e.ctrlKey &&
-                e.key === "z"
-            ){
-
-                e.preventDefault();
-
-
-                this.core.undo();
-
-
-                console.log(
-                    "CTRL + Z"
-                );
-
-            }
-
-
-
-            /*
-            =========================
-             REDO
-            =========================
-            */
-
-
-            if(
-                e.ctrlKey &&
-                e.key === "y"
-            ){
-
-                e.preventDefault();
-
-
-                this.core.redo();
-
-
-                console.log(
-                    "CTRL + Y"
-                );
-
-            }
-
-
-
-            /*
-            =========================
-             DELETE OBJETO
-            =========================
-            */
-
-
-            if(
-                e.key === "Delete"
-            ){
-
-                e.preventDefault();
+                /*
+                =========================
+                 UNDO
+                =========================
+                */
 
 
                 if(
-                    this.core.modules.actions
+                    e.ctrlKey &&
+                    e.key === "z"
                 ){
 
-                    this.core.modules.actions.deleteSelected();
+                    e.preventDefault();
+
+
+                    this.core.undo();
+
+
+                    console.log(
+                        "CTRL + Z"
+                    );
 
                 }
 
 
-                console.log(
-                    "DELETE"
-                );
+
+
+
+                /*
+                =========================
+                 REDO
+                =========================
+                */
+
+
+                if(
+                    e.ctrlKey &&
+                    e.key === "y"
+                ){
+
+                    e.preventDefault();
+
+
+                    this.core.redo();
+
+
+                    console.log(
+                        "CTRL + Y"
+                    );
+
+                }
+
+
+
+
+
+                /*
+                =========================
+                 DELETE OBJETO
+                =========================
+                */
+
+
+                if(
+                    e.key === "Delete"
+                ){
+
+                    e.preventDefault();
+
+
+                    if(
+                        this.core.modules.actions
+                    ){
+
+                        this.core.modules.actions.deleteSelected();
+
+                    }
+
+
+                    console.log(
+                        "DELETE"
+                    );
+
+                }
+
+
+
+
+
+                /*
+                =========================
+                 DUPLICAR OBJETO
+                 CTRL + D
+                =========================
+                */
+
+
+                if(
+                    e.ctrlKey &&
+                    e.key === "d"
+                ){
+
+                    e.preventDefault();
+
+
+                    if(
+                        this.core.modules.actions
+                    ){
+
+                        this.core.modules.actions.duplicateSelected();
+
+                    }
+
+
+                    console.log(
+                        "CTRL + D"
+                    );
+
+                }
+
+
 
             }
 
-
-        }
-
-    );
+        );
 
 
-}
+    }
+
 
 } // <-- CIERRA LA CLASE KeyboardManager
 
