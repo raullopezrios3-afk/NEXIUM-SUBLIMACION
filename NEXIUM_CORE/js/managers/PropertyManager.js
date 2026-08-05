290fd6e40f15874844a4474f9b9ee92b618a863c
raullopezrios3-afk <raullopezrios3@gmail.com>
2026-08-05 16:12:14 -0600

    Create PropertyManager.js

---
+++ NEXIUM_CORE/js/managers/PropertyManager.js
@@ -0,0 +1,170 @@
+/*
+=========================================
+ NEXIUM CORE
+ Property Manager
+=========================================
+*/
+
+
+class PropertyManager{
+
+
+    constructor(core){
+
+        this.core = core;
+
+
+        this.container =
+        document.getElementById(
+            "object-properties"
+        );
+
+
+        console.log(
+            "Property Manager creado"
+        );
+
+
+    }
+
+
+
+
+
+    /*
+    =========================================
+     ACTUALIZAR PROPIEDADES
+    =========================================
+    */
+
+
+    update(){
+
+
+        if(!this.container)
+        return;
+
+
+
+        const obj =
+        this.core.selectedObject;
+
+
+
+        if(!obj){
+
+
+            this.container.innerHTML =
+            "Seleccione un objeto";
+
+
+            return;
+
+        }
+
+
+
+        this.container.innerHTML = `
+
+
+            <div class="property">
+
+                <label>
+                    ID
+                </label>
+
+                <input 
+                    value="${obj.id}"
+                    disabled
+                >
+
+            </div>
+
+
+
+            <div class="property">
+
+                <label>
+                    Tipo
+                </label>
+
+                <input 
+                    value="${obj.tipo}"
+                    disabled
+                >
+
+            </div>
+
+
+
+            <div class="property">
+
+                <label>
+                    X
+                </label>
+
+                <input 
+                    id="propX"
+                    value="${obj.x}"
+                >
+
+            </div>
+
+
+
+            <div class="property">
+
+                <label>
+                    Y
+                </label>
+
+                <input 
+                    id="propY"
+                    value="${obj.y}"
+                >
+
+            </div>
+
+
+
+            <div class="property">
+
+                <label>
+                    Ancho
+                </label>
+
+                <input 
+                    id="propAncho"
+                    value="${obj.ancho}"
+                >
+
+            </div>
+
+
+
+            <div class="property">
+
+                <label>
+                    Alto
+                </label>
+
+                <input 
+                    id="propAlto"
+                    value="${obj.alto}"
+                >
+
+            </div>
+
+
+        `;
+
+
+    }
+
+
+
+}
+
+
+window.PropertyManager =
+PropertyManager;
