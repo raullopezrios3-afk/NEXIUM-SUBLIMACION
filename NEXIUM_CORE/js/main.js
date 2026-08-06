c08f10871dafc4ebc6687f5b0e446c5368829867
raullopezrios3-afk <raullopezrios3@gmail.com>
2026-08-05 18:03:45 -0600

    Initialize Nexium engine and create test rectangle
    Removed commented-out code and added initialization for the Nexium engine and rectangle creation.

--- NEXIUM_CORE/js/main.js
+++ NEXIUM_CORE/js/main.js
@@ -5,49 +5,35 @@
 =========================================
 */
 
-
 document.addEventListener(
 "DOMContentLoaded",
 ()=>{
 
-
     const nexium =
     new NexiumCore();
-
 
     window.NEXIUM =
     nexium;
 
+    // Inicializar el motor
+    nexium.init();
 
-
+    // Inicializar módulos
     nexium.modules.canvas.init(
         "nexiumCanvas"
     );
 
-
-
     nexium.modules.mouse.bindEvents();
 
+    // OBJETO DE PRUEBA
+    nexium.createRectangle(
+        100,
+        100,
+        200,
+        100
+    );
 
-
-  /*
-=========================================
-OBJETO DE PRUEBA
-(Deshabilitado)
-=========================================
-
-nexium.createRectangle(
-    100,
-    100,
-    200,
-    100
-);
-*/
-
-
-
+    // Iniciar render
     nexium.modules.render.start();
-
-
 
 });
